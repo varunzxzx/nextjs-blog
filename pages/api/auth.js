eccf1fbaf2fd715b8df79bbf13f94f0267989bdb
Varun <murugan.varun@sap.com>
2020-11-02 16:37:31 +0530

    update netlify

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -2,5 +2,5 @@ const client_id = "140ebdfe5e3aef6f4d22";
 const authUrl = `https://github.com/login/oauth/authorize?client_id=${client_id}&scope=repo,user`;
 
 export default (req, res) => {
-  res.redirect(authUrl);
+  return res.redirect(authUrl);
 };
